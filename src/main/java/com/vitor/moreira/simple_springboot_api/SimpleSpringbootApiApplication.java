e96796202c6338fff5757ea6d0a55673f44a6526
VitorMoo <viitornmoreira@outlook.com>
2024-07-04 10:57:24 -0300

    first commit

---
+++ src/main/java/com/vitor/moreira/simple_springboot_api/SimpleSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.moreira.simple_springboot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitor/moreira/simple_springboot_api/SimpleSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.moreira.simple_springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
