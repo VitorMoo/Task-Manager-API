a1d0235bdc50f40360fea084acf699bf1c4d08b6
VitorMoo <viitornmoreira@outlook.com>
2024-07-04 14:34:24 -0300

    feat : created the User Class and updated some dependencies

---
+++ src/main/java/com/vitor/moreira/simple_springboot_api/model/CreateUser.java
@@ -0,0 +1,4 @@
+package com.vitor.moreira.simple_springboot_api.model;
+
+public interface CreateUser {
+}
---
+++ src/main/java/com/vitor/moreira/simple_springboot_api/model/User.java
@@ -0,0 +1,83 @@
+package com.vitor.moreira.simple_springboot_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table
+public class User {
+    public static final String TABLE_NAME ="user";
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id",unique = true)
+    private Integer id;
+
+    @Column(name = "username",length = 100,nullable = false,unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(min = 4,max = 50, groups = CreateUser.class)
+    private String username;
+
+
+    @Column(name = "password")
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = {CreateUser.class,UpdateUser.class})
+    @Size(groups = {CreateUser.class,UpdateUser.class} , min = 8,max = 50)
+    private String password;
+
+    //private List<Task> tasks = new Arraylist<Task>();
+
+    public User() {
+    }
+
+    public User(Integer id,String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null)? 0 : this.id.hashCode());
+        return result;
+    }
+}
