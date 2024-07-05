ff5607d09d609621b1b973d4ef5ad10796f5b0ec
VitorMoo <viitornmoreira@outlook.com>
2024-07-05 12:01:11 -0300

    feat : created User Service class

---
+++ src/main/java/com/vitor/moreira/simple_springboot_api/service/UserService.java
@@ -0,0 +1,56 @@
+package com.vitor.moreira.simple_springboot_api.service;
+
+import com.vitor.moreira.simple_springboot_api.model.CreateUser;
+import com.vitor.moreira.simple_springboot_api.model.User;
+import com.vitor.moreira.simple_springboot_api.repository.TaskRepository;
+import com.vitor.moreira.simple_springboot_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Integer id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(()-> new RuntimeException(
+                "usuario nao encontrado! id:" + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        obj.setId(null);
+
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+    
+    public void delete(Integer id){
+        findById(id);
+        try{
+            this.userRepository.deleteById(id);
+        }catch (Exception e){
+            throw new RuntimeException("nao e possivel excluir," +
+                    "entidades relacionadas");
+        }
+    }
+}
