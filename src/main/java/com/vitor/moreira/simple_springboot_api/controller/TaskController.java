7898df318b763738219d89bceb9ffe2edb10029c
VitorMoo <viitornmoreira@outlook.com>
2024-07-10 08:52:26 -0300

    feat : created the Task controller class

---
+++ src/main/java/com/vitor/moreira/simple_springboot_api/controller/TaskController.java
@@ -0,0 +1,50 @@
+package com.vitor.moreira.simple_springboot_api.controller;
+
+
+import com.vitor.moreira.simple_springboot_api.model.Task;
+import com.vitor.moreira.simple_springboot_api.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/task")
+@Validated
+public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Integer id){
+        Task task = this.taskService.findById(id);
+        return ResponseEntity.ok(task);
+    }
+
+    public ResponseEntity<Task> create(@Valid @RequestBody Task task){
+        this.taskService.create(task);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(task.getId()).toUri();
+        return  ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody Task obj, @PathVariable Integer id) {
+        obj.setId(id);
+        this.taskService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
