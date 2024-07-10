4c3d027b7218aedf17a31a680aa013c8d4f27052
VitorMoo <viitornmoreira@outlook.com>
2024-07-10 08:53:33 -0300

    refactor : updated the class

--- src/main/java/com/vitor/moreira/simple_springboot_api/service/TaskService.java
+++ src/main/java/com/vitor/moreira/simple_springboot_api/service/TaskService.java
@@ -18,23 +18,36 @@ public class TaskService {
     @Autowired
     private UserService userService;
 
-    public Task findById(Integer id){
+    public Task findById(Integer id) {
         Optional<Task> task = this.taskRepository.findById(id);
-        return task.orElseThrow(()-> new RuntimeException(
+        return task.orElseThrow(() -> new RuntimeException(
                 "Tarefa nao encontrada id:" + id + ", Tipo: " +
                         Task.class.getName()
         ));
     }
 
     @Transactional
-    public Task create(Task obj){
+    public Task create(Task obj) {
         User user = this.userService.findById(obj.getUser().getId());
         obj.setId(null);
         obj.setUser(user);
         obj = this.taskRepository.save(obj);
-                return obj;
+        return obj;
     }
 
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
 
-
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
 }
