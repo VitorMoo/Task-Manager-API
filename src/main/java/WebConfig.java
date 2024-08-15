e0c3665a1edf2e30298bf3fe835fb578e58a5da9
VitorMoo <viitornmoreira@outlook.com>
2024-08-15 09:15:50 -0300

    feat: create the WebConfig class

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -1,5 +1,3 @@
-package config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    public void addCorsMapping(CorsRegistry registry){
-        registry.addMapping("/**");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") 
+                .allowedOrigins("http://127.0.0.1:5500")  
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*")  
+                .allowCredentials(true);  
     }
-
 }
