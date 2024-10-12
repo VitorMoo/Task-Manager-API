d7460677a1c70a929ffcefbe92663378361d78b1
VitorMoo <viitornmoreira@outlook.com>
2024-10-11 21:29:14 -0300

    feat: adding authorization logic

--- src/main/java/com/vitor/moreira/simple_springboot_api/service/TaskService.java
+++ src/main/java/com/vitor/moreira/simple_springboot_api/service/TaskService.java
@@ -1,14 +1,19 @@
 package com.vitor.moreira.simple_springboot_api.service;
 
+import com.vitor.moreira.simple_springboot_api.jwt.UserSpringSecurity;
 import com.vitor.moreira.simple_springboot_api.model.Task;
 import com.vitor.moreira.simple_springboot_api.model.User;
+import com.vitor.moreira.simple_springboot_api.model.enums.ProfileEnum;
 import com.vitor.moreira.simple_springboot_api.repository.TaskRepository;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.AuthorizationException;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.DataBindingViolationException;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
 @Service
 public class TaskService {
@@ -20,16 +25,33 @@ public class TaskService {
     private UserService userService;
 
     public Task findById(Integer id) {
-        Optional<Task> task = this.taskRepository.findById(id);
-        return task.orElseThrow(() -> new RuntimeException(
-                "Tarefa nao encontrada id:" + id + ", Tipo: " +
-                        Task.class.getName()
-        ));
+        Task task = this.taskRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+                "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()));
+
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+        if (Objects.isNull(userSpringSecurity)
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !userHasTask(userSpringSecurity, task))
+            throw new AuthorizationException("Acesso negado!");
+
+        return task;
+    }
+
+    public List<Task> findByUser_Id() {
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+        if (Objects.isNull(userSpringSecurity))
+            throw new AuthorizationException("Acesso negado!");
+
+        List<Task> tasks = this.taskRepository.findByUser_Id(userSpringSecurity.getId());
+        return tasks;
     }
 
     @Transactional
     public Task create(Task obj) {
-        User user = this.userService.findById(obj.getUser().getId());
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+        if (Objects.isNull(userSpringSecurity))
+            throw new AuthorizationException("Acesso negado!");
+
+        User user = this.userService.findById(userSpringSecurity.getId());
         obj.setId(null);
         obj.setUser(user);
         obj = this.taskRepository.save(obj);
@@ -37,10 +59,10 @@ public class TaskService {
     }
 
     @Transactional
-    public void update(Task obj) {
+    public Task update(Task obj) {
         Task newObj = findById(obj.getId());
         newObj.setDescription(obj.getDescription());
-        this.taskRepository.save(newObj);
+        return this.taskRepository.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -48,12 +70,12 @@ public class TaskService {
         try {
             this.taskRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+            throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas!");
         }
     }
 
-    public List<Task> findAll(Integer userId) {
-        return this.taskRepository.findAll();
+    private Boolean userHasTask(UserSpringSecurity userSpringSecurity, Task task) {
+        return task.getUser().getId().equals(userSpringSecurity.getId());
     }
 
 }
--- src/main/java/com/vitor/moreira/simple_springboot_api/service/UserService.java
+++ src/main/java/com/vitor/moreira/simple_springboot_api/service/UserService.java
@@ -1,13 +1,20 @@
 package com.vitor.moreira.simple_springboot_api.service;
 
+import com.vitor.moreira.simple_springboot_api.jwt.UserSpringSecurity;
 import com.vitor.moreira.simple_springboot_api.model.User;
 import com.vitor.moreira.simple_springboot_api.model.enums.ProfileEnum;
 import com.vitor.moreira.simple_springboot_api.repository.UserRepository;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.AuthorizationException;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.DataBindingViolationException;
+import com.vitor.moreira.simple_springboot_api.service.exceptions.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,43 +23,53 @@ import java.util.stream.Stream;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private PasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
 
-    public User findById(Integer id){
+    public User findById(Integer id) {
+        UserSpringSecurity userSpringSecurity = authenticated();
+        if (!Objects.nonNull(userSpringSecurity)
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecurity.getId()))
+            throw new AuthorizationException("Acesso negado!");
+
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow(()-> new RuntimeException(
-                "usuario nao encontrado! id:" + id + ", Tipo: " + User.class.getName()
-        ));
+        return user.orElseThrow(() -> new ObjectNotFoundException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
     }
 
     @Transactional
-    public User create(User obj){
+    public User create(User obj) {
         obj.setId(null);
-        obj.setPassword(passwordEncoder.encode(obj.getPassword()));
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
 
     @Transactional
-    public User update(User obj){
-
+    public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
-        newObj.setPassword(this.passwordEncoder.encode(obj.getPassword()));
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
-    
-    public void delete(Integer id){
+
+    public void delete(Integer id) {
         findById(id);
-        try{
+        try {
             this.userRepository.deleteById(id);
-        }catch (Exception e){
-            throw new RuntimeException("nao e possivel excluir," +
-                    "entidades relacionadas");
+        } catch (Exception e) {
+            throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+
+    public static UserSpringSecurity authenticated() {
+        try {
+            return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
         }
     }
 }
